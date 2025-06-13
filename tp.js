a737774b166e54c7a0b24b33e45968277f3d18a8
guest-elife <guestelife@gmail.com>
2025-06-13 11:41:46 +0100

    Delete tp.js

--- tp.js
+++
@@ -1,7 +0,0 @@
-const button = document.getElementById("change-text");
-const message = document.getElementById("message");
-
-button.addEventListener("click", () => {
-  message.innerText = "🎉 Le message a été changé avec JavaScript !";
-  console.log("Le bouton a été cliqué !");
-});
