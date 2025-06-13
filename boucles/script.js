c03194ea4fdcb0cea749a48fd6e010058fc5b86a
Roua Zeiri <rouazeiri30187a57-la@gomycode.tn>
2025-06-13 11:39:24 +0100

    Delete boucles directory

--- boucles/script.js
+++
@@ -1,7 +0,0 @@
- let resultat = "";
-    for (let i = 0; i <= 20; i++) {
-      if (i % 2 === 0) {
-        resultat += i + " ";
-      }
-    }
-    document.getElementById("boucle").innerText = resultat;
